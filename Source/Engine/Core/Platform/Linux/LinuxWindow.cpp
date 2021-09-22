a91b7e78119d0752dc51dd525b278b5228c192eb
DezlowNG <dezlow@bk.ru>
2021-09-22 16:52:07 +0700

    added linux platform support

---
+++ Source/Engine/Core/Platform/Linux/LinuxWindow.cpp
@@ -0,0 +1,34 @@
+//
+// Created by dezlow on 22.09.2021.
+//
+
+#ifdef __linux__
+
+#include <stdexcept>
+#include "LinuxWindow.h"
+
+WindowProperties WindowHnd::CreateWindow(unsigned int width, unsigned int height, const char* title)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    WindowProperties windowProps {
+            glfwCreateWindow((int)width, (int)height, title, nullptr, nullptr),
+            (int)width, (int)height, title
+    };
+
+    if (windowProps.window == nullptr)
+        std::runtime_error("Failed to create window!");
+
+    glfwMakeContextCurrent(windowProps.window);
+    glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        std::runtime_error("Failed to init glad!");
+
+    return windowProps;
+}
+
+#endif
---
+++ Source/Engine/Core/Platform/Linux/LinuxWindow.h
@@ -0,0 +1,20 @@
+//
+// Created by dezlow on 22.09.2021.
+//
+
+#ifndef REBIRTH_LINUXWINDOW_H
+#define REBIRTH_LINUXWINDOW_H
+
+#ifdef __linux__
+
+#include "Engine/Core/Platform/Platform.h"
+
+class WindowHnd
+{
+public:
+    static WindowProperties CreateWindow(unsigned int width, unsigned int height, const char* title);
+};
+
+#endif
+
+#endif //REBIRTH_LINUXWINDOW_H
