530f21d40688083c54d82eac63762be52d5059c4
DezlowNG <dezlow@bk.ru>
2021-10-03 19:36:58 +0700

    updated examples

--- examples/simplenovel/script.lua
+++ examples/simplenovel/script.lua
@@ -1,16 +1,21 @@
-local img = Image("bus_stop.jpg")
-local img2 = Image("ext_aidpost_day.jpg")
-local spr2 = Image("shizyka_smile.png")
+local bus_stop = Image("bus_stop.jpg")
+local ext_aidpost_day = Image("ext_aidpost_day.jpg")
+local ext_bus = Image("ext_bus.jpg")
 local spr = Sprite("normal.png")
+local shizyka_smile = Sprite("shizyka_smile.png")
+
+local TestCharacter = Character("Test Character")
+local Dezlow = Character("Dezlow")
 
 function label_start()
-    scene(img)
-    spr2:show()
-    text("Welcome to Rebirth Engine!")
-    spr2:hide()
-    text("Your ad could be here...")
-    scene(img2)
+    scene(ext_aidpost_day)
     spr:show()
-    text("Your ad could be here...")
+    TestCharacter "Hello, World!"
     spr:hide()
+    TestCharacter "Hello, World! Hello, World!"
+    scene(bus_stop)
+    shizyka_smile:show()
+    Dezlow "Hello, World!"
+    scene(ext_bus)
+    Dezlow "Hello, World!"
 end
